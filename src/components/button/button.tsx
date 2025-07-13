f83c4a6a7e84d513612fe633359e8be22db16472
a.karpenko <oksud22@gmail.com>
2025-07-13 18:12:58 +0300

    fixed issue with button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React from 'react';
+import classNames from 'classnames';
 
-import {
-	BUTTON_COLOR_TO_STYLES_MAP,
-	BUTTON_SIZE_TO_STYLES_MAP
-} from './constants';
-import {
-	ButtonColor,
-	ButtonSize
-} from './types';
-import { ExtendedReactFunctionalComponent } from "src/types";
+import { BUTTON_COLOR_TO_STYLES_MAP, BUTTON_SIZE_TO_STYLES_MAP } from './constants';
+import { ButtonColor, ButtonSize } from './types';
+import { ExtendedReactFunctionalComponent } from 'src/types';
 
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-	size?: ButtonSize;
-	color?: ButtonColor;
+export interface ButtonProps
+  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  size?: ButtonSize;
+  color?: ButtonColor;
 }
 
 export const Button: ExtendedReactFunctionalComponent<ButtonProps> = ({
-	color = 'primary', size = 'medium', children, ...props
+  color = 'primary',
+  size = 'medium',
+  children,
+  className,
+  ...props
 }) => {
-	const { disabled } = props;
+  const { disabled } = props;
 
-	const sizes = BUTTON_SIZE_TO_STYLES_MAP[size];
-	const colors = BUTTON_COLOR_TO_STYLES_MAP[disabled ? 'disabled' : color];
+  const sizes = BUTTON_SIZE_TO_STYLES_MAP[size];
+  const colors = BUTTON_COLOR_TO_STYLES_MAP[disabled ? 'disabled' : color];
 
-	return <button className={`button-style-hover cursor-pointer border-none rounded-1  p-2 text-center ${sizes} ${colors}`} {...props}>{children}</button>;
-}
+  return (
+    <button
+      className={classNames(
+        `button-style-hover cursor-pointer border-none rounded-1  p-2 text-center ${sizes} ${colors}`,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
